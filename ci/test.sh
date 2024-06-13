1705083da67faf59e685f1ddc3c197d407adb902
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2024-06-13 19:49:39 +0200

    start on v2

--- ci/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-RESULT=$(curl http://localhost:3031/alive)
-
-if [ "${RESULT}" != "alive!" ]; then
-  echo "Not alive!"
-  exit 1;
-fi
-echo "${RESULT}"
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
-#[tokio::main]
-async fn main() {
-    tiauth::prepare_server().await;
-    tiauth::run_server().await;
+use redb::{Database, Error, ReadableTable, TableDefinition};
+use opaque_borink::server::register_server;
+
+struct Login {
+
+}
+
+const TABLE: TableDefinition<&str, u64> = TableDefinition::new("my_data");
+
+fn get() -> Result<(), Error> {
+    let db = Database::create("my_db.redb")?;
+    let write_txn = db.begin_write()?;
+    {
+        let mut table = write_txn.open_table(TABLE)?;
+        table.insert("my_key", &123)?;
+    }
+    write_txn.commit()?;
+
+    let read_txn = db.begin_read()?;
+    let table = read_txn.open_table(TABLE)?;
+    println!("{}", table.get("my_key")?.unwrap().value());
+
+    Ok(())
+}
+
+fn main() {
+    println!("hello");
+
+    get().unwrap();
 }
